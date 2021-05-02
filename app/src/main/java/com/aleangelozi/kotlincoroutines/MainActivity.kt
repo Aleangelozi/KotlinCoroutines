3ab86d83aad24b21c25406f3cb0fc0537bb96461
aleangelozi <aleangelozi@gmail.com>
2021-05-02 09:30:13 -0300

    Migration to View Binding.

--- app/src/main/java/com/aleangelozi/kotlincoroutines/MainActivity.kt
+++ app/src/main/java/com/aleangelozi/kotlincoroutines/MainActivity.kt
@@ -3,15 +3,21 @@ package com.aleangelozi.kotlincoroutines
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.aleangelozi.kotlincoroutines.databinding.ActivityMainBinding
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
 private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
         // All coroutines inside GlobalScope will live as long
         // as the application (Main Thread) does. GlobalScope starts a new thread.
