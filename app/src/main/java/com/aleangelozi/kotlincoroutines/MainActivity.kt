8a0033f5c76877ac34acda744a3264a97604a78d
aleangelozi <aleangelozi@gmail.com>
2021-05-02 10:22:28 -0300

    Adding suspend functions.

--- app/src/main/java/com/aleangelozi/kotlincoroutines/MainActivity.kt
+++ app/src/main/java/com/aleangelozi/kotlincoroutines/MainActivity.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.aleangelozi.kotlincoroutines.databinding.ActivityMainBinding
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 private const val TAG = "MainActivity"
@@ -23,10 +24,30 @@ class MainActivity : AppCompatActivity() {
         // as the application (Main Thread) does. GlobalScope starts a new thread.
         GlobalScope.launch {
 
+            // delay() will only suspend the current coroutine, not the whole thread.
+            delay(3000)
             Log.d(TAG, "Coroutine says hi from thread ${Thread.currentThread().name}")
+
+            val doSomethingAnswer = doSomething()
+            val doSomethingAnswer2 = doSomething2()
+
+            Log.d(TAG, doSomethingAnswer)
+            Log.d(TAG, doSomethingAnswer2)
+
 
         }
 
         Log.d(TAG, "Hi from thread ${Thread.currentThread().name}")
+    }
+
+    // We also can create customized suspend functions.
+    private suspend fun doSomething(): String {
+        delay(3000)
+        return "This is the answer from doSomething."
+    }
+
+    private suspend fun doSomething2(): String {
+        delay(3000)
+        return "This is the answer from doSomething2."
     }
 }
