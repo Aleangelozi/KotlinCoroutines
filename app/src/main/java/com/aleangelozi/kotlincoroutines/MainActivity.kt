02cd2199e0775b310d207b8e5da93d7e7bd67084
aleangelozi <aleangelozi@gmail.com>
2021-06-01 23:22:29 -0300

    Saving

--- app/src/main/java/com/aleangelozi/kotlincoroutines/MainActivity.kt
+++ app/src/main/java/com/aleangelozi/kotlincoroutines/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.aleangelozi.kotlincoroutines
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import com.aleangelozi.kotlincoroutines.databinding.ActivityMainBinding
-import kotlinx.coroutines.*
-import kotlin.system.measureTimeMillis
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 private const val TAG = "MainActivity"
 
@@ -19,86 +22,27 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
-        // Coroutines are executed sequentially by default, but we can change
-        // in order to execute asynchronously.
+        binding.button.setOnClickListener {
+            //Comment the next first line of code and uncomment the next second line of code
+            //to observe the difference.
 
-        // Running sequentially
-        GlobalScope.launch(Dispatchers.IO) {
-            val time = measureTimeMillis {
-                val answer1 = networkCall1()
-                val answer2 = networkCall2()
-
-                Log.d(TAG, "Answer1 is $answer1")
-                Log.d(TAG, "Answer2 is $answer2")
+            lifecycleScope.launch {
+            //GlobalScope.launch {
+                while (true) {
+                    delay(1000L)
+                    Log.d(TAG, "is still running ...")
+                }
             }
-            Log.d(TAG, "First request took $time ms")
 
-
-        }
-
-        // Running asynchronously using Jobs.
-        // This is not recommended, it's better practice to use Async.
-        GlobalScope.launch(Dispatchers.IO) {
-            val time = measureTimeMillis {
-                lateinit var answer3: String
-                lateinit var answer4: String
-
-                val job1 = launch { answer3 = networkCall3() }
-                val job2 = launch { answer4 = networkCall4() }
-                job1.join()
-                job2.join()
-
-                Log.d(TAG, "Answer3 is $answer3")
-                Log.d(TAG, "Answer4 is $answer4")
+            GlobalScope.launch {
+                delay(5000L)
+                Intent(this@MainActivity, SecondActivity::class.java).also {
+                    startActivity(it)
+                    finish()
+                }
             }
-            Log.d(TAG, "Second request took $time ms")
         }
 
 
-        // Running asynchronously using Async.
-        GlobalScope.launch(Dispatchers.IO) {
-            val time = measureTimeMillis {
-
-
-                val answer5 = async { networkCall5() }
-                val answer6 = async {networkCall6() }
-
-                Log.d(TAG, "Answer3 is ${answer5.await()}")
-                Log.d(TAG, "Answer4 is ${answer6.await()}")
-            }
-            Log.d(TAG, "Second request took $time ms")
-        }
     }
-
-    private suspend fun networkCall1(): String {
-        delay(3000L)
-        return "Answer 1"
-    }
-
-    private suspend fun networkCall2(): String {
-        delay(3000L)
-        return "Answer 2"
-    }
-
-    private suspend fun networkCall3(): String {
-        delay(3000L)
-        return "Answer 3"
-    }
-
-    private suspend fun networkCall4(): String {
-        delay(3000L)
-        return "Answer 4"
-    }
-
-    private suspend fun networkCall5(): String {
-        delay(3000L)
-        return "Answer 5"
-    }
-
-    private suspend fun networkCall6(): String {
-        delay(3000L)
-        return "Answer 6"
-    }
-
-
 }
